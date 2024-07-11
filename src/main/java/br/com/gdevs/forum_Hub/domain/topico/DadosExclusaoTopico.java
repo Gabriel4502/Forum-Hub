17fae1792d90769281ccc48df3d02ab0a89c6b1c
gabriel4502 <gabrielmessias3@hotmail.com>
2024-07-11 12:15:22 -0300

    criação dto de exclusao de usuario

--- src/main/java/br/com/gdevs/forum_Hub/domain/topico/DadosExclusaoTopico.java
+++ src/main/java/br/com/gdevs/forum_Hub/domain/topico/DadosExclusaoTopico.java
@@ -2,8 +2,9 @@ package br.com.gdevs.forum_Hub.domain.topico;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record DadosExclusaoTopico (@NotNull @JsonAlias("id") Long topicoId, @NotBlank @Valid String email, String senha) {
+public record DadosExclusaoTopico (@NotNull @JsonAlias("id") Long topicoId, @NotBlank @Email String email, String senha) {
 }
